3f24ec2f8cc69afc6fd32bae6afb5dc349c5cee0
Adriel Café <ac@adrielcafe.com>
2014-06-23 08:55:36 -0300

    ...

--- src/com/adrielcafe/minhaescola/SplashScreenActivity.java
+++ src/com/adrielcafe/minhaescola/SplashScreenActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Window;
 
 public class SplashScreenActivity extends Activity {
     private static int SPLASH_TIME_OUT = 3000;
@@ -11,6 +12,9 @@ public class SplashScreenActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+		getActionBar().hide();
 		setContentView(R.layout.activity_splashscreen);
 		
 		new Handler().postDelayed(new Runnable() {
